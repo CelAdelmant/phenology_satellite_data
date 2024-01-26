34b3c28a8070d85185eb250ad55249679360aa7a
nilomr <nilomerinorecalde@gmail.com>
2024-01-26 15:22:10 +0000

    continue refactoring processing step

--- scripts/main.R
+++ scripts/main.R
@@ -135,6 +135,10 @@ get_hdf_files <- function(mcd_quadrant) {
         pattern = "*.hdf",
         full.names = TRUE
     )
+    # throw an error if there are any .aux.xml files
+    if (length(grep("\\.aux\\.xml$", hdf_files)) > 0) {
+        stop("Found unwanted .aux.xml files in", mcd_quadrant)
+    }
     # Throw an error if no HDF files found
     if (length(hdf_files) == 0) {
         stop("No HDF files found in", mcd_quadrant)
@@ -142,10 +146,17 @@ get_hdf_files <- function(mcd_quadrant) {
     return(hdf_files)
 }
 
+print_time_elapsed <- function(result) {
+    time_elapsed <- result[3]
+    minutes <- floor(time_elapsed / 60)
+    seconds <- round(time_elapsed %% 60, 2)
+    message("Finished in ", minutes, " minutes and ", seconds, " seconds")
+}
 
 
 # ──── PREPROCESS HDF FILES ───────────────────────────────────────────────────
 # Here, I create rasters from the HDF files downloaded from EarthDataSearch
+
 
 # Get the list of MCD quadrants
 mcd_quadrants <- list.dirs(
@@ -153,10 +164,8 @@ mcd_quadrants <- list.dirs(
     full.names = TRUE, recursive = FALSE
 )
 
-# Main processing step
-
 # Loop through each MCD quadrant, creating a df of all layers for each quadrant,
-# HDF file, year and layer
+# HDF file, year and layer.
 
 dfs <- list()
 
@@ -172,6 +181,14 @@ for (mcd_quadrant in mcd_quadrants) {
 
 layers <- dplyr::as_tibble(do.call(rbind, dfs))
 
+# create a test subset of the layers (just 3 random rows)
+layers <- layers[sample(nrow(layers), 3), ] # REVIEW
+
+# save layers to a csv file in the derived data folder
+layers_file <- file.path(config$path$derived_data, "tmp", "layers.csv")
+# create the directory if it doesn't exist
+if (!dir.exists(dirname(layers_file))) dir.create(dirname(layers_file))
+write.csv(layers, layers_file, row.names = FALSE)
 
 
 # Now we have a dataframe with all the layers for each HDF file,
@@ -184,11 +201,15 @@ progressr::with_progress({
     p <- progressr::progressor(
         steps = nrow(layers), enable = TRUE, trace = FALSE
     )
-    furrr::future_map(seq_len(nrow(layers)), function(i) {
-        row_cont <- layers[i, ]
-        p(message = row_cont$message)
-        save_layer(row_cont, datum, output_dir = output_dir)
-    }, .options = furrr::furrr_options(seed = TRUE))
+    time <- system.time({
+        result <- furrr::future_map(seq_len(nrow(layers)), function(i) {
+            row_cont <- layers[i, ]
+            p(message = row_cont$message)
+            save_layer(row_cont, datum, output_dir = output_dir)
+        }, .options = furrr::furrr_options(seed = TRUE))
+    })
+    # Pretty print time elapsed
+    print_time_elapsed(time)
 })
 
 
@@ -196,6 +217,41 @@ progressr::with_progress({
 # The output is 10 .tif files per hdf file, each depicting a single layer of the multilayer raster hdf files
 # These are all saved in their respective folders (mcd Quadrant --> year --> //)
 # I then use these layers for the analysis below
+
+
+# ──── SOURCE FOR BELOW ───────────────────────────────────────────────────────
+
+#' Generate PNG image of cropped raster
+#'
+#' This function generates a PNG image of a cropped raster for a specific site, variable, and year.
+#'
+#' @param raster_cropped The cropped raster object.
+#' @param contour The contour object used for cropping.
+#' @param site_name The name of the site.
+#' @param variable The variable being plotted.
+#' @param year The year being plotted.
+#' @param quadrant_folder The folder name for the quadrant.
+#' @param output_dir The output directory where the PNG image will be saved.
+#'
+#' @return None
+#'
+#' @examples
+#' generate_png(raster_cropped, contour, "Site A", "Temperature", 2022, "Quadrant 1", "/path/to/output")
+generate_png <- function(raster_cropped, contour, site_name,
+                         variable, year, quadrant_folder, output_dir) {
+    # Save a png of the cropped raster for reference
+    rviz <- terra::disagg(raster_cropped, 30) |> terra::crop(contour, mask = TRUE)
+
+    title <- paste(site_name, variable, year)
+    png_name <-
+        file.path(
+            output_dir, quadrant_folder,
+            paste0(gsub(" ", "_", title), ".png")
+        )
+    grDevices::png(png_name)
+    terra::plot(rviz, main = title, col = rev(grDevices::terrain.colors(255)))
+    dev.off()
+}
 
 
 
@@ -223,6 +279,127 @@ site_quadrant <- read.csv(file.path(
     "site_name_quadrant.csv"
 ))
 
+# read layers list
+layers_file <- file.path(config$path$derived_data, "tmp", "layers.csv")
+layers <- read.csv(layers_file) |> dplyr::as_tibble()
+
+
+# add the paths to the derived projected tif files to the layers dataframe
+layers <- layers |>
+    dplyr::mutate(
+        path = file.path(
+            config$path$derived_data, "satellite",
+            quadrant_name, year, paste0(file_name, "_", layer, "_proj.tif")
+        )
+    )
+
+# add the site_code to the layers dataframe, matching the number after the _ in quadrant_name to 'quadrant' in site_quadrant and extracting the site_code
+
+# make a new 'quadrant' column in the layers dataframe by extracting the number(s) after the _ in quadrant_name
+layers <- layers |>
+    dplyr::mutate(
+        quadrant = as.integer(gsub(".*_(\\d+)$", "\\1", quadrant_name))
+    )
+
+# based on the quadrant number, get the site_code from the site_quadrant dataframe
+layers <- layers |>
+    dplyr::left_join(site_quadrant, by = "quadrant") |>
+    dplyr::select(-quadrant)
+
+#REVIEW: need to iterate over sites, because multiple sites share quadrants.
+# maybe build a list of dataframes, one for all the years*layers for each site,
+# and then combine them at the end? site always will poit to the same shapefile.
+
+
+
+# now we have the site name, we can get the path to the shapefile, which is in the raw/shapefiles directory, with extension .kml, and starts with the site name
+# 1. get the list of shapefiles
+shapefiles
+
+# 2,. for each file_name, get the path to the corresponding shapefile and add it to that row, based on the site name:
+layers <- layers |>
+    dplyr::mutate(
+        shapefile = sapply(site, function(x) {
+            shapefile <- shapefiles |> grep(paste0("^", x), value = TRUE)
+            if (length(shapefile) == 0) {
+                stop("No shapefile found for ", x)
+            }
+            if (length(shapefile) > 1) {
+                stop("Multiple shapefiles found for ", x)
+            }
+            shapefile
+        })
+    )
+
+
+
+
+# check that all the paths exist
+if (any(!file.exists(layers$path))) {
+    stop("Some files are missing")
+}
+
+
+
+for (i in seq_len(nrow(layers))) {
+    row <- layers[i, ]
+}
+
+    # Extracting the variable (Greenup, MidGreenup, MidGreendown etc)
+    variable <- row$layer
+
+    # get the site_name from the site_quadrant dataframe
+    # first 3 characters of the file name
+    site <- substr(row$file_name, 1, 3)
+
+    # Reading the raster file
+    raster_data <- terra::rast(row$path, datum)
+
+    # REVIEW: remove duplicate variables
+    raster_data <- raster_data[[1]]
+
+    # Reading in kml and converting to shapefile
+    contour <- shapefile |>
+        sf::st_read() |>
+        sf::st_zm(drop = TRUE) |>
+        terra::vect()
+
+
+    # REVIEW: had to reproject the contour to match the raster?
+    # check if destination crs is always the same and remove this step
+    contour <- terra::project(contour, terra::crs(raster_data))
+
+
+    # Cropping and masking the raster with the contour,
+    # calculating the cell area and the proportion of the cell
+    # covered by the woodland contour
+    raster_cropped <- terra::crop(raster_data, contour, mask = TRUE)
+    cell_size_km <- terra::cellSize(raster_cropped, unit = "km")
+    raster_combined <- c(raster_cropped, cell_size_km)
+    values <- terra::extract(raster_combined, contour, exact = TRUE)
+    values <- values |>
+        dplyr::mutate(
+            cell_number = seq_len(nrow(values)),
+            year = year,
+            variable = variable,
+            site = site_name,
+            quadrant = quadrant_number
+        )
+    names(values)[grep(paste0(".*", variable, ".*"), names(values))] <- "value"
+
+    # append this to the quadrant dataframe
+    quadrant_dfs[[length(quadrant_dfs) + 1]] <- values
+
+    # Save a png of the cropped raster for reference
+    generate_png(
+        raster_cropped, contour, site_name, variable, year,
+        quadrant_folder, output_dir
+    )
+}
+
+
+
+
 # defining list of years
 years <- as.character(seq(2001, 2021))
 
@@ -235,19 +412,43 @@ all_years_data_list_wa <- list() # this is for the weighted average datasets
 
 # Nested for loops to extract information from the raster sub-datasets
 
+data = list()
+
 for (shapefile in shapefiles) {
     # Extracting name of shapefile
     shapefile_name <- tools::file_path_sans_ext(basename(shapefile))
+
+    # Reading in kml and converting to shapefile
+    contour <- shapefile |>
+        sf::st_read() |>
+        sf::st_zm(drop = TRUE) |>
+        terra::vect()
+
     # Looking which quadrant it falls into
     site_name <- substr(shapefile_name, 1, 3)
     site_quadrant_row <- site_quadrant |> dplyr::filter(site_code == site_name)
     quadrant_number <- site_quadrant_row$quadrant
     quadrant_folder <- paste0("quadrant_", quadrant_number)
 
+    # check if there are any files in the quadrant folder,
+    # otherwise skip to next quadrant
+    if (length(list.files(file.path(output_dir, quadrant_folder))) == 0) {
+        message("No files found for ", quadrant_folder)
+        next
+    }
+
+    quadrant_dfs <- list()
 
     for (year in years) {
         # listing tif files within the quadrant folder of interest
-        tif_files <- list.files(file.path(output_dir, quadrant_folder, year), pattern = "*proj.tif", full.names = TRUE)
+        tif_files <- list.files(file.path(output_dir, quadrant_folder, year),
+            pattern = "*proj.tif", full.names = TRUE
+        )
+        # check if there are any tif files, otherwise skip to next year
+        if (length(tif_files) == 0) {
+            message("No tif files found for ", year, " in ", quadrant_folder)
+            next
+        }
         # creating a new empty dataset for each year
         combined_data_peryear <- data.frame()
 
@@ -260,90 +461,58 @@ for (shapefile in shapefiles) {
             # Reading the raster file
             raster_data <- terra::rast(tif_file, datum)
 
-            # Reading in kml and converting to shapefile
-            contour <- sf::st_read(shapefile)
-            contour <- sf::st_zm(contour, drop = TRUE) # removing the z dimension
-            contour <- terra::vect(contour) # making this a spatial object
+            # REVIEW: remove duplicate variables
+            raster_data <- raster_data[[1]]
 
             # REVIEW: had to reproject the contour to match the raster?
             contour <- terra::project(contour, terra::crs(raster_data))
 
-            # draw the contour over the raster_data
-            # crop raster data to the bounding box of the contour + 20 km
-            # (to make sure the contour is fully covered)
 
-            # Cropping and masking the raster with the contour
+            # Cropping and masking the raster with the contour,
+            # calculating the cell area and the proportion of the cell
+            # covered by the woodland contour
             raster_cropped <- terra::crop(raster_data, contour, mask = TRUE)
-            raster_polygons <- terra::as.polygons(raster_cropped)
-            terra::plot(raster_cropped)
-            terra::plot(raster_polygons)
+            cell_size_km <- terra::cellSize(raster_cropped, unit = "km")
+            raster_combined <- c(raster_cropped, cell_size_km)
+            values <- terra::extract(raster_combined, contour, exact = TRUE)
+            values <- values |>
+                dplyr::mutate(
+                    cell_number = seq_len(nrow(values)),
+                    year = year,
+                    variable = variable,
+                    site = site_name,
+                    quadrant = quadrant_number
+                )
+            names(values)[grep(paste0(".*", variable, ".*"), names(values))] <- "value"
 
+            # append this to the quadrant dataframe
+            quadrant_dfs[[length(quadrant_dfs) + 1]] <- values
 
-            raster_polygons$cell_number <- seq_len(nrow(raster_polygons)) # assigning cell number
-
-
-            # Making spatial data frame
-            raster_polygons <- SpatialPolygonsDataFrame(raster_polygons, data.frame(raster_polygons), match.ID = TRUE)
-
-
-            # REVIEW: NMR - Not sure what this does
-
-            # Calculating intersect and adding to new dataset
-            intersect <- sf::st_intersection(raster_polygons, contour) # calculating intersect
-
-
-            intersect_data <- intersect@data # making a new dataset of only intersected polygons
-            intersected_rows <- intersect$cell_number # finding intersected rows
-            area_overall <- area(raster_polygons) # assigning area to matrix
-            area_overall <- area_overall[intersected_rows] # using only intersected rows
-            intersect_data$area_overall <- area_overall # assigning to dataset
-            intersect_data$area_overlap <- area(intersect) # finding area of intersection
-            intersect_data$proportion_wood <- intersect_data$area_overlap / intersect_data$area_overall # calculating proportion of area covered in 'site'
-            intersect_data$year <- year # assigning year value in column from file name
-            colnames(intersect_data)[3] <- variable # changing the name of the value column to the variable name (so I can merge columns later)
-
-            # Assigning this data to a new temporary dataframe
-            temporary_data <- intersect_data[, c("cell_number", variable)]
-
-            # Creating or merging dataframe/s
-            if (nrow(combined_data_peryear) == 0) {
-                combined_data_peryear <- temporary_data # Initialize combined_data_peryear with the first 'temp_data' set
-            } else {
-                # Merge 'temp_data' into 'combined_data_peryear' based on 'cell_number'
-                combined_data_peryear <- merge(combined_data_peryear, temporary_data, by = "cell_number", all = TRUE)
-            }
-
-            # Merging with description data
-            description_data <- intersect_data[, c("Name", "Description", "cell_number", "area_overall", "area_overlap", "proportion_wood", "year")]
-            combined_data_peryear_full <- merge(combined_data_peryear, description_data, by = "cell_number", all = TRUE)
-
-            # This creates a dataset with all information about the pixels which intersect with the contour.
-            # This is raw data and therefore must be cleaned and re-formatted below
+            # Save a png of the cropped raster for reference
+            generate_png(
+                raster_cropped, contour, site_name, variable, year,
+                quadrant_folder, output_dir
+            )
         }
-        # Doing some manipulations to transform "day since 1970" to "day of year"
-        combined_data_peryear_full <- combined_data_peryear_full |>
-            mutate(days_between = as.numeric(difftime(
-                as.Date(paste0(year, "-01-01"), format = "%Y-%m-%d"),
-                as.Date("1970-01-01"),
-                units = "days"
-            ))) |>
-            mutate(
-                Greenup_doy = Greenup - days_between,
-                MidGreenup_doy = MidGreenup - days_between,
-                MidGreendown_doy = MidGreendown - days_between,
-                Dormancy_doy = Dormancy - days_between,
-                Maturity_doy = Maturity - days_between,
-                Peak_doy = Peak - days_between,
-                Senescence_doy = Senescence - days_between
-            )
 
-        # Removing unwated columns
-        combined_data_peryear_full <- combined_data_peryear_full |>
-            select(
-                -Dormancy, -MidGreenup, -Greenup, -MidGreendown, -Peak,
-                -Senescence, -Maturity, -Description, -area_overall,
-                -area_overlap, -days_between
+
+
+
+        values <- values |>
+            dplyr::mutate(
+                days_between = as.numeric(difftime(as.Date(paste0(year, "-01-01")),
+                    as.Date("1970-01-01"),
+                    units = "days"
+                )),
+                dplyr::across(dplyr::starts_with(layer_names),
+                    ~ . - days_between,
+                    .names = "doy"
+                )
             )
+        # remove 'days_between' and variable columns using base R
+        values[, !(names(values) %in% c("days_between", variable))]
+
+
 
         # Saving dataset to raw list created above
         all_years_data_list_raw[[year]] <- combined_data_peryear_full
